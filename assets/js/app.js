6a06612c426687294588bb060de4eee1e7558aac
Gaby Trejo <gabytrejo303@gmail.com>
2017-03-23 15:53:27 -0600

    Habilitación de escritura con teclado

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,13 @@ function resolverOperacion(operacion){
   var resultado = document.getElementById('pantalla');
   resultado.value = eval(resultado.value);
 }
+
+function numerosTeclado(event){
+  var tecla = event.keyCode;
+  if (tecla == 13) {
+    resolverOperacion();
+  }
+  if(tecla < 42 || tecla > 57){
+      return false;
+  }
+}
